2b150105ee2f438c3727901efc9b01f4cd604e80
DavidLucas1122 <davidlucased@gmail.com>
2026-02-17 10:22:02 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -1,90 +1,171 @@
-'use strict'
+"use strict";
 
-const container_inicio = document.getElementById('inicio')
-const container_cursos = document.getElementById('curso')
-const container_aluno = document.getElementById('aluno')
+const container_inicio = document.getElementById("inicio");
+const container_cursos = document.getElementById("curso");
+const container_aluno = document.getElementById("aluno");
 
-const container_alunos = document.getElementById('alunos')
+const container_alunos = document.getElementById("alunos");
 
-const titulo_curso = document.getElementById('title')
+const titulo_curso = document.getElementById("title");
 
-const buttons_cursos = document.getElementById('buttons_cursos')
+const buttons_cursos = document.getElementById("buttons_cursos");
+const button_close = document.getElementById("button_close");
+const span_close = document.getElementById("span_close");
 
-
-async function pegarCursos() {
-    const url = `https://lion-school-phbo.onrender.com/cursos`
-    const response = await fetch(url)
-    const dados = await response.json()
-
-    return dados
+function fecharJanela() {
+  window.close();
 }
 
-async function mostrarCursos () {
-    const cursos = await pegarCursos()
+function voltarInicio() {
+  span_close.textContent = "Sair";
+  container_inicio.style.display = "flex";
+  container_cursos.style.display = "none";
+  container_aluno.style.display = "none";
 
-    
-    cursos.forEach(curso => {
-        const button = document.createElement('button')
+  button_close.removeEventListener("click", voltarInicio);
+  button_close.addEventListener("click", fecharJanela);
+}
 
-        button.textContent = curso.sigla
+function voltarCursos() {
+  container_cursos.style.display = "flex";
+  container_aluno.style.display = "none";
 
-        buttons_cursos.appendChild(button)
+  button_close.removeEventListener("click", voltarCursos);
+  button_close.addEventListener("click", voltarInicio);
+}
 
-        button.addEventListener('click', function () {
-            mostrarAlunos(curso.id)
-            titulo_curso.textContent = curso.nome
-            container_inicio.style.display = 'none'
-            container_cursos.style.display = 'flex'
-        })
-    })
+button_close.addEventListener("click", fecharJanela);
+
+async function pegarCursos() {
+  const url = `https://lion-school-phbo.onrender.com/cursos`;
+  const response = await fetch(url);
+  const dados = await response.json();
+
+  return dados;
+}
+
+async function mostrarCursos() {
+  const cursos = await pegarCursos();
+
+  cursos.forEach((curso) => {
+    const button = document.createElement("button");
+
+    button.textContent = curso.sigla;
+
+    buttons_cursos.appendChild(button);
+
+    button.addEventListener("click", function () {
+      mostrarAlunos(curso.id);
+
+      span_close.textContent = "Voltar";
+      button_close.removeEventListener("click", fecharJanela);
+      button_close.addEventListener("click", voltarInicio);
+
+      titulo_curso.textContent = curso.nome;
+      container_inicio.style.display = "none";
+      container_cursos.style.display = "flex";
+    });
+  });
 }
 
 async function pegarAlunos(cursoId) {
-    const url = `https://lion-school-phbo.onrender.com/alunos?curso_id=${cursoId}`
-    const response = await fetch(url)
-    const dados = await response.json()
+  const url = `https://lion-school-phbo.onrender.com/alunos?curso_id=${cursoId}`;
+  const response = await fetch(url);
+  const dados = await response.json();
 
-    return dados
+  return dados;
 }
 
 async function mostrarAlunos(cursoId) {
-    container_alunos.replaceChildren()
-    const alunos = await pegarAlunos(cursoId)
+  container_alunos.replaceChildren();
+  const alunos = await pegarAlunos(cursoId);
 
-    console.log(alunos)
+  alunos.forEach((aluno) => {
+    const div = document.createElement("div");
+    div.classList.add("aluno");
 
-    alunos.forEach(aluno => {
-        const div = document.createElement('div')
-        div.classList.add('aluno')
+    const img = document.createElement("img");
+    img.src = aluno.foto;
 
-        const img = document.createElement('img')
-        img.src = aluno.foto
+    const p = document.createElement("p");
+    p.textContent = aluno.nome.toUpperCase();
 
-        const p = document.createElement('p')
-        p.textContent = aluno.nome.toUpperCase()
+    div.append(img, p);
+    container_alunos.append(div);
 
-        div.append(img, p)
-        container_alunos.append(div)
+    div.addEventListener("click", function () {
+      mostrarInfosAluno(aluno.id);
 
-        div.addEventListener('click', function() {
-            mostrarInfosAluno(aluno.id)
-            container_cursos.style.display = 'none'
-            container_aluno.style.display = 'flex'
-        })
-    })
+      button_close.removeEventListener("click", voltarInicio);
+      button_close.addEventListener("click", voltarCursos);
+
+      container_cursos.style.display = "none";
+      container_aluno.style.display = "flex";
+    });
+  });
 }
 
 async function buscarInfosAluno(alunoId) {
-    const url = `https://lion-school-phbo.onrender.com//alunos/${alunoId}`
-    const response = await fetch(url)
-    const dados = await response.json()
+  const url = `https://lion-school-phbo.onrender.com/alunos/${alunoId}`;
+  const response = await fetch(url);
+  const dados = await response.json();
 
-    return dados
+  return dados;
 }
 
-function mostrarInfosAluno (idAluno){
-    
+async function mostrarInfosAluno(idAluno) {
+  container_aluno.replaceChildren();
+
+  const infosAluno = await buscarInfosAluno(idAluno);
+
+  const alunoInfos = document.createElement("div");
+  alunoInfos.classList.add("aluno_infos");
+
+  const fotoAluno = document.createElement("img");
+  fotoAluno.src = infosAluno.foto;
+
+  const nome = document.createElement("h2");
+  nome.textContent = infosAluno.nome.toUpperCase();
+
+  alunoInfos.append(fotoAluno, nome);
+
+  const divDesempenho = document.createElement("div");
+  divDesempenho.classList.add("aluno_stats");
+
+  infosAluno.desempenho.forEach((status) => {
+    const coluna = document.createElement("div");
+    coluna.classList.add("stat");
+
+    const categoria = document.createElement("p");
+    categoria.textContent = status.categoria;
+
+    const barra = document.createElement("div");
+    barra.classList.add("barra");
+
+    const valor = document.createElement("div");
+    valor.classList.add("valor");
+    valor.style.height = `${status.valor * 3}px`;
+
+    const numero = document.createElement("p");
+    numero.textContent = status.valor;
+
+    if (status.valor <= 30) {
+      valor.style.backgroundColor = "#C11010";
+      numero.style.color = "#C11010";
+    } else if (status.valor < 51) {
+      valor.style.backgroundColor = "#E5B657";
+      numero.style.color = "#E5B657";
+    } else {
+      valor.style.backgroundColor = "#3347B0";
+      numero.style.color = "#3347B0";
+    }
+
+    barra.append(numero, valor, categoria);
+    coluna.append(barra);
+    divDesempenho.append(coluna);
+  });
+
+  container_aluno.append(alunoInfos, divDesempenho);
 }
 
-
-mostrarCursos()
+mostrarCursos();
